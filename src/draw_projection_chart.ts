55f3faedecf2abb2f72b0ab5f0e7f683c6049465
Michael Kreil <github@michael-kreil.de>
2026-02-12 11:52:54 +0100

    chore: standardize font sizes in drawColumnChart and drawProjectionChart functions

--- src/draw_projection_chart.ts
+++ src/draw_projection_chart.ts
@@ -19,14 +19,11 @@ export function drawProjectionChart(query: string, data: ProcessedEntry[]): void
 	const y0 = height - margin.bottom;
 	const y1 = margin.top;
 
-	const baseFontSize = Math.max(8, Math.min(10, width / 80)) + 'px';
-	const axisFontSize = Math.max(10, Math.min(14, width / 57)) + 'px';
-
 	const svg = d3
 		.create('svg')
 		.attr('viewBox', [0, 0, width, height])
 		.style('font-family', 'sans-serif')
-		.style('font-size', baseFontSize);
+		.style('font-size', '10px');
 
 	const x = d3.scaleBand(
 		data.map((d) => d.index),
@@ -34,7 +31,6 @@ export function drawProjectionChart(query: string, data: ProcessedEntry[]): void
 	);
 
 	svg.append('g')
-		.style('font-size', axisFontSize)
 		.attr('transform', `translate(0,${y0})`)
 		.call(d3.axisBottom(x).tickSize(0).tickFormat(indexToLabel))
 		.selectAll('text')
@@ -47,7 +43,6 @@ export function drawProjectionChart(query: string, data: ProcessedEntry[]): void
 		.range([y0, y1]);
 
 	svg.append('g')
-		.style('font-size', axisFontSize)
 		.attr('transform', `translate(${x0},0)`)
 		.call(d3.axisLeft(y).tickFormat(formatCurrency).ticks(5));
 
@@ -133,7 +128,7 @@ export function drawProjectionChart(query: string, data: ProcessedEntry[]): void
 			.attr('text-anchor', 'end')
 			.text(formatCurrency(difference))
 			.attr('font-weight', 'bold')
-			.attr('font-size', '1.5em')
+			.attr('font-size', '1.2em')
 			.attr('fill', colorRed);
 	}
 
